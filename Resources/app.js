98defbf85544734e42c25b84d5c44bb7ce7c57be
Andrew Yenney <andrewyenney@Andrews-MacBook-Pro.local>
2015-04-05 14:19:42 -0700

    nu

--- Resources/app.js
+++ Resources/app.js
@@ -24,8 +24,25 @@ function setupSettings(){
 	return view;
 }
 
-
 winSettings.add(setupSettings());
 tabGroup.addTab(tabSettings);
 
 tabGroup.open();
+
+<?xml version="1.0" encoding="UTF-8"?>
+<projectDescription>
+	<name>Hackahon4</name>
+	<comment></comment>
+	<projects>
+	</projects>
+	<buildSpec>
+		<buildCommand>
+			<name>com.appcelerator.titanium.core.builder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+	</buildSpec>
+	<natures>
+		<nature>com.appcelerator.titanium.mobile.nature</nature>
+	</natures>
+</projectDescription>
